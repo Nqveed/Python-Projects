43562717632a40ae39af648c2ead4f3c2b838551
Nqveed <muhammadnaveedashraf10@gmail.com>
2025-04-10 07:16:30 +0500

    Add files via upload

---
+++ 2_createnumpyarray.py
@@ -0,0 +1,22 @@
+import numpy as np
+x = np.array([1,2,3,4,5])
+print(x)
+print(type(x))
+# import numpy as np
+# y = np.array((1,2,3,4,5))
+# print(y)
+# import numpy as np
+# z = np.array(42)
+# print(z)
+# import numpy as np
+# r = np.array([[1,2,3],[4,5,6]])
+# print(r)
+# import numpy as np
+# s = np.array([[[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]]])
+# print(s)
+# # print(s.ndim)
+# import numpy as np
+# f =  np.array([1,2,3,4,5],ndmin=5)
+# print(f)
+# print('number of dimsions:', f.ndim)
+
