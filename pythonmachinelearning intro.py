b4b440b7a39df46dc172678bfbcaadd9c03acddb
Nqveed <muhammadnaveedashraf10@gmail.com>
2025-04-10 07:19:57 +0500

    Add files via upload

---
+++ pythonmachinelearning intro.py
@@ -0,0 +1,7 @@
+# Machine learning is making the computer to learn from studying data and statistics.This is a program that to analyses data and learns to predict the outcome.
+# Where to start: here for calcution important numbers based on data sets we use mathematics and for studying statistics and using the various python modules,
+# Data set: [99,86,87,88,111,86,103,87,94,78,77,85,87]
+# Data Type: There are three types of main category of data type in ML.:
+#(1) Numerical: 1st; Discrete Date: numbers that are limited to integer like numbers cars passing by road. 2nd ; Continuous Data: numbers that are of infinite values like price of an item.
+#(2) Categorical Data:The values that can not be count or measured like color.
+#(3) Ordinal Data: these are like categorical data but can be measured or countable like school grades Where A is better than B. 
