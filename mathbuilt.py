394874b72351b4b9158305aef7df4df6a3527f5b
Nqveed <muhammadnaveedashraf10@gmail.com>
2025-04-10 07:02:14 +0500

    Add files via upload

---
+++ mathbuilt.py
@@ -0,0 +1,27 @@
+#  python has extensive maths built - in function,
+# # Built - in maths function
+# x =min(5,16,8)
+# y= max(5,16,78)
+# print(x)
+# print(y)
+# #  abs()- this function return absolute positive value
+# z= abs(-8)
+# print(z)
+# # pow( x,y) - this function return the values of x to the power y,
+# x =pow(2,5)
+# print(x)
+# we can  also import math module externally
+# import math
+# x= math.sqrt(25)
+# print(x) 
+# math.cell() and math.floor()
+# import math
+# x=math.ceil(1.4)
+# y= math.floor(1.4)
+# print(x)
+# print(y)
+# import math
+# x= math.pi
+# print(x)
+# math module reference for python web site
+
