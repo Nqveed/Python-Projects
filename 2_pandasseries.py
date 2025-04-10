5d917734521bb4cca04268703ef2093e7f8e8cb5
Nqveed <muhammadnaveedashraf10@gmail.com>
2025-04-10 07:08:47 +0500

    Add files via upload

---
+++ 2_pandasseries.py
@@ -0,0 +1,43 @@
+# import pandas as pd
+# a = [3,7,8,11]
+# b = pd.Series(a)
+# print(b)
+# import pandas as pd
+# c = [3,7,8,11]
+# d = pd.Series(c)
+# print(d[3])
+import pandas as pd
+f = [3,7,8,11]
+h = pd.Series(f,index=["w","x","y","z"])
+print(h)
+# cal = {"day1": 360,"day2": 400,"day3": 450,"day4": 500}
+# a = pd.Series(cal)
+# print(a)
+# import pandas as pd
+# f = {"cal":[350,400,450,500],"duration":[35,40,50,55]}
+# h = pd.DataFrame(f)
+# print(h)
+# import pandas as pd
+# a= [3,7,8,11]
+# b= pd.Series(a)
+# print(b)
+# import pandas as pd
+# a= [3,7,8,11]
+# b= pd.Series(a)
+# print(b[0])
+# import pandas as pd
+# a= [3,7,8,11]
+# b= pd.Series(a,index=["w","x", "y" , "z"])
+# print(b)
+# import pandas as pd
+# cal = {"day1":420,"day2":320,"day3":390}
+# c=pd.Series(cal)
+# print(c)
+# import pandas as pd
+# cal = {"day1":420,"day2":320,"day3":390}
+# result = pd.Series(cal,index=["day1","day3"])
+# print(result)
+# import pandas as pd
+# x=[4,6,9,12,16]
+# y=pd.Series(x, index=["1","2","3","4","5"])
+# print(y["3"])
