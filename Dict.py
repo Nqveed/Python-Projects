3652969c8f1efb6d79ca46f47d6a448eb92a62d9
Nqveed <muhammadnaveedashraf10@gmail.com>
2025-04-10 06:57:29 +0500

    Add files via upload

---
+++ Dict.py
@@ -0,0 +1,81 @@
+# defination  of dictionary is ordered,indexed changable ,dulicate are not allowed
+x={"brand":"ford" ,"model":"DT","year":2024,"year":2024} 
+# print(x)
+# print(x["year"])
+# print(len(x))
+# y= {"brand":"ford" ,"model":"DT","year":2025,"year":2024,"color":["red","white","blue"]}
+# print(y)
+# print(type(y))
+# print(x["brand"])
+# s = x.get("model")
+# print(s)
+# t=x["model"]
+# print(t)
+# a=x.keys()
+# print(a)
+# adding items in dictionary
+# x["color"]="white"
+# print(x)
+# a=x.values()
+# print(a)
+# r=x.values()
+# print(x)
+# x["color"]="red"
+# print(x)
+r=x.items()
+print(r)
+# if "model" in x:
+#     print("yes,'model'is present in the x")
+# x["year"]=2026
+# print(x)
+# x.update({"year":204})
+# print(x)
+# x["color"]="red"
+# print(x)
+# x.update({"color":"green"})
+# print(x)
+# removing items from  the dictionary
+# x.pop("model")
+# print(x)
+# del x ["year"]
+# print(x)
+# del x
+# print(x)
+# d={"a":1,"b":2}
+# x.clear()
+# print(x)
+# copy in Dictionary
+# y= x.copy()
+# print(y)
+# y=dict(x)
+# print(y)
+# nested dicttionary, dict can be to contain another dict
+# myfamily={"child1":{"name":"ram","year":10},"child2":{"name":"laxmam","year":8},"child3":{"name":"bharat","year":6} }
+# print(myfamily)
+# if you want to add 3 dict into a new dict,
+# child1={"name":"ram","year":10}
+# child2={"name":"laxmam","year":8}
+# child3={"name":"bharat","year":6} 
+# myfamily1={"child1":child1,"child2":child2,"child3":child3}
+# print(myfamily1)
+# clear()
+# copy()
+# setdefault()
+# keys()
+# fromkeys()
+# pop()
+# pop item()
+# get()
+# values()
+# items()
+# update()
+
+
+
+
+
+
+
+
+
+
