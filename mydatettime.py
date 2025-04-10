497dca354767adc04e4555a082c417705ca2adcf
Nqveed <muhammadnaveedashraf10@gmail.com>
2025-04-10 07:02:58 +0500

    Add files via upload

---
+++ mydatettime.py
@@ -0,0 +1,47 @@
+# import datetime
+# a=datetime.datetime.now()
+# print(a)
+# import datetime
+# b= datetime.datetime.now()
+# print(b.year)
+# print(b.strftime("%A"))
+# # Creating the date objects - you can use datetime()
+# # for creating the date
+# import datetime
+# c = datetime.datetime(2025,1,5)
+# print(c)
+# The strftime () method
+# import datetime
+# d= datetime.datetime(2024,12,18)
+# print(d.strftime("%B"))
+import datetime
+f=datetime.datetime(2025,1,27)
+print(f.strftime("%U"))
+import datetime
+h=datetime.datetime.now()
+print(h.strftime("%x"))
+# reference of all the legal format date codes
+#a =weekday = wed
+#  %A = weekday fullform
+#  %w -  wednesday as a number -3 
+#  %d - days 
+#  %B- month as December fullform
+#  %b short month
+#  %m month as number as 12
+#  %y 24
+#  %Y 2024
+#  %H  hour 24
+#  %I - 12th formate 05 
+#  %p  am /mp
+#  %M mimute
+#  %S seconds
+#  %f micro seconds - 000000
+#  %Z- timezon
+#   %j day number of year 365
+# %U  week- number as 52
+# %C century - 2000 
+#  %x local ver of date  05/15/2024
+# python strftime.org web
+
+
+
