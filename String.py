f6de81a94665a79b0a9341eb63d8c24f8d6723c2
Nqveed <muhammadnaveedashraf10@gmail.com>
2025-04-10 06:54:00 +0500

    Add files via upload

---
+++ String.py
@@ -0,0 +1,70 @@
+x ="Naveed,Ashraf "
+y='Naveed'
+z='Ashraf'
+a='Muhammad'
+# print(a[:5])
+# print(x)
+# print(y)
+# print(x[0:4])
+# print(y[:-2])
+# print(z[5:])
+# print(z[:5])
+# print(z[-6:-2])
+# # a ="Naveed Ashraf"
+# b ="banana"
+# for b in "banana":
+#     print(b)
+# print(len(x)) 
+# # Check string
+txt = "In this tutorial everything is free"  
+print ("free" in txt)
+if "free"in txt:
+    print("yes,'free' is present,")
+# print("naveed" not in txt)
+# if "naveed"not in txt:
+#     print("No, 'naveed is  not present")   
+#  string cilicing
+# print(x[:9])
+# print(x[7:])
+# b="hello world!"
+# print(b[-5:-2])
+# Modifying string
+# print(x.upper())
+# print(x.lower())
+# print(x.strip())
+# a= "This is my tutorial"
+# print(a.replace( "m","n"))
+# b=x.split(",")
+# print(b)
+# a="Muhammad"
+# b="Naveed"
+# c="Ashraf"
+# e= a+b
+# print(e)
+# d=a + " " + b+" "+c
+# print(d)
+# Age =36
+# txt="My name is  Naveed and age {}"
+# print(txt.format(Age))
+# f='Naveed'
+# l='Ashraf'
+# age=36
+# print(f'My first name {f} & My list name {l} & age  is  {age}')
+# print(f)
+# qty=3
+# items=600
+# price=49.90
+# myorder="I want {} pieces of item number {} for {} Rupee,"
+# print(myorder.format(qty,items,price))
+
+# qty=3
+# items=600
+# price=49.90
+# myorder="I want to pay {2} Rupees for {0} pieces of item number {1} ,"
+# print(myorder.format(qty,items,price))
+
+
+
+  
+
+
