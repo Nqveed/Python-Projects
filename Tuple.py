85bffc7b044fa07fe7e94cc9c4cc1c11cc4da57d
Nqveed <muhammadnaveedashraf10@gmail.com>
2025-04-10 06:58:12 +0500

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,64 @@
+x=("apple","banana","orange","mango","cherry","papaya")
+# print(x)
+# print(len(x))
+# x=("Naveed",)
+# tuple data type(string,int,boolean)
+# y=(1,4,7,8,11)
+# z=(True,False,True)
+# r=("apple",34,True,"honda",23.8)
+#  tuple () constructor
+# z= tuple(("apple","banana","orange","mango","cherry"))
+# print(z)
+# print(x[3])
+# print(x[:3])
+# print(x[2:6])
+# print(x[3:])
+# print(x[-2])
+# print(x[-5:-2])
+# print(x[:-3])
+# print(x[-3:])
+# if "mango"in x:
+    # print("yes,'mango' is in x")
+# changing of tuple values    
+w=("apple","banana","orange","mango","cherry","papaya") 
+# print(type(w)) 
+# v=list(w)
+# print(type(v)) 
+# v[1]="guava"
+# w=tuple(v)
+# print(w)
+# print(type(w))
+# v.append("grape")
+# w=tuple(v)
+# print(w)
+# adding tuple to atuple
+# t = ("apple","banana","orange")
+# s=("cherry",)
+# t+= s
+# print(t)
+# v=list(w)
+# v.remove("banana")
+# w=tuple(v)
+# print(w)
+# r=("banana","grape" )
+# del(r)
+#unpacking the tuple (when we exract the vslue from the tuple)
+# x= ("Naveed","apple","Adnan")
+# (green,yellow,red)=x
+# print(green)
+# print(yellow)
+# print(red)
+# x=("Naveed","apple","Adnan","cherry","banana","guava")
+# (green,yellow,red,*blue)=x
+# print(green)
+# print(yellow)
+# print(red)
+# print(blue)
+# joining tuple
+# x=("a","b","c")
+# y=(1,2,3)
+# z=x+y
+# print(z)
+# fruits=["Naveed","banana","orange","cherry","mango","papaya","kiwi"]
+# s=fruits*2
+# print(s)
