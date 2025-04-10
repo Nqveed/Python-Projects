beef9c8ad207f373df9dfffa8b057030767e5f2d
Nqveed <muhammadnaveedashraf10@gmail.com>
2025-04-10 06:56:49 +0500

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,85 @@
+# sets in python are unordered,unchanged unindexed
+# a = {"apple","banana","cherry","grape",7,1}
+# print(a)
+# a.add("guava")
+# print(a)
+# b={"mango","papaya","guava","grape",7}
+# print(b)
+# print(type(x))
+# # # dulicate  not allowed in sets
+# c = {"a","b","c","a"}
+# print(c)
+# print(type(c))
+# print(len(c))
+d={"apple",12,True,14,False}
+print(d)
+# # The set constructor
+f=set(("banana","grape",15,True))
+print(f)
+print(type(f))
+for i in f:
+    print(i)
+print("apple" in f)  
+# x.update(z)
+# print(x)
+# # x.remove(1)
+# # print(x)
+# z.discard("mango")
+# print(z)
+# f= {1,3,"apple"}
+# f.clear()
+# print(f)
+# join two sets 
+# y=x.union(z)
+# print(y)
+# x.update(z)
+# print(x)
+# x.intersection_update(z)
+# print(x)
+# y=x.intersection(z)
+# print(y)
+# x.symmetric_difference_update(z)
+# print(x)
+# y=x.symmetric_difference(z)
+# print(y)
+# All python buit in methods
+# add()
+# clear()
+# copy
+# intersection()
+# discard()
+# remove()
+# update()
+# union()
+# symmetric()
+# difference()
+# symmetric_difference()
+# symmetric_difference_update()
+# dulicate()
+# disjoin()
+# subsets()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
